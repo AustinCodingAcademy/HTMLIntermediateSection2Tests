e812b858a8beb40d35fc73b0e84844faa60b7e9d
Peter Arevalo <peteywiggin@gmail.com>
2015-09-21 17:22:28 -0500

    finished lesson 2 tests

--- 01LessonOne/LessonOne.js
+++ 01LessonOne/LessonOne.js
@@ -2,36 +2,62 @@ module.exports = function() {
 
   this.returnTrue = function () {
     // should return true
-
+	  return true;
+      
   }
 
   this.returnFalse = function() {
     // should return false
+	  return false;
 
   }
 
   this.greaterThan = function (big, small) {
     // should return true if the first argument is greater than the second argument
     // and return false if the first argument is less than the second argument
+	  if (big > small) {
+		  return true;
+	  }
+	  else {
+		  return false;
+	  }
 
   }
 
   this.lessThan = function (small, big) {
     // should return true if the first argument is less than the second argument
     // and return false if the first argument is greater than the second argument
+	  if (small < big) {
+		  return true;
+	  }
+	  else {
+		  return false;
+	  }
 
   }
 
   this.equalTo = function (a, b) {
     // should return true if the first argument is equal to the second argument
     // and should return true if the first argument is equal to the second argument
-    return a === b;
+	  if (a === b) {
+		  return true;
+	  }
+	  else {
+		  return false;
+	  }
 
   }
 
   this.notEqualTo = function (a, b) {
     // should return true if the first argument is not equal to the second argument
     // and should return false if the first argument is equal to the second argument
+	  if (a !== b) {
+		  return true;
+	  }  
+	  else if (a === b){
+	      return false;
+	  }  
 
   }
+
 }
--- 02LessonTwo/LessonTwo.js
+++ 02LessonTwo/LessonTwo.js
@@ -1,14 +1,27 @@
 module.exports = function() {
 
-  this.whichSpecies = function (character) {
-    // should return "dog" when character is 'scooby'
-    // should return "cat" when character is 'garfield'
-    // should return "fish" when character is 'nemo'
-    // should return false if character is anything else
-  }
+	this.whichSpecies = function (character) {
+		  if (character === "scooby") {
+			  return "dog";
+		  } 
+		  if (character === "garfield") {
+			  return "cat";
+		  }
+		  if (character === "nemo") {
+			  return "fish";
+		  } else {
+			  return false;
+		  }
+	  };
 
   this.isEven = function (number) {
     // should return true is number is even (divisible by 2)
+	  if (number % 2 == 0) {
+		  return true;
+	  }
+	  else {
+		  return false;
+	  }
   }
 
 }
