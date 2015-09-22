0fdf333c5075426a917e1933eeb5dda103c66776
reneedudley <4reneedudley@gmail.com>
2015-09-22 10:57:35 -0500

    passed lesson one, biatch

--- 01LessonOne/LessonOne.js
+++ 01LessonOne/LessonOne.js
@@ -2,24 +2,34 @@ module.exports = function() {
 
   this.returnTrue = function () {
     // should return true
-
+    return true;
   }
 
   this.returnFalse = function() {
     // should return false
-
+    return false;
   }
 
   this.greaterThan = function (big, small) {
     // should return true if the first argument is greater than the second argument
+    if (big>small){
+      return true;
+    }
     // and return false if the first argument is less than the second argument
-
+    else{
+      return false;
+    }
   }
 
   this.lessThan = function (small, big) {
     // should return true if the first argument is less than the second argument
+    if (small<big){
+      return true;
+    }
     // and return false if the first argument is greater than the second argument
-
+    else{
+      return false;
+    }
   }
 
   this.equalTo = function (a, b) {
@@ -31,7 +41,11 @@ module.exports = function() {
 
   this.notEqualTo = function (a, b) {
     // should return true if the first argument is not equal to the second argument
-    // and should return false if the first argument is equal to the second argument
-
+    if (a !== b){
+      return true;
+    }
+    else{
+      return false;
+    }
   }
 }
