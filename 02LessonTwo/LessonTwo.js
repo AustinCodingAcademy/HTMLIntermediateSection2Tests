99f17759efbef10cb0df33ea5416f06d34eef9b7
reneedudley <4reneedudley@gmail.com>
2015-09-22 11:07:49 -0500

    can't believe i put in a fucking colon after return

--- 02LessonTwo/LessonTwo.js
+++ 02LessonTwo/LessonTwo.js
@@ -2,13 +2,29 @@ module.exports = function() {
 
   this.whichSpecies = function (character) {
     // should return "dog" when character is 'scooby'
+    switch (character){
+    	case 'scooby':
+    		return "dog"
+    		break;
+    	case 'garfield':
+    		return "cat"
+    		break;
+    	case 'nemo':
+    		return "fish"
+    		break; 
+    	default: 
+    		return false;
+    };
     // should return "cat" when character is 'garfield'
     // should return "fish" when character is 'nemo'
     // should return false if character is anything else
-  }
+  };
 
   this.isEven = function (number) {
     // should return true is number is even (divisible by 2)
+    if (number % 2 === 0){
+    	return true;
+    }
   }
 
 }
