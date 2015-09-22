33c9ad656901cf2646fe6b6d4d45a8f775dc867d
Chen Yang <yangc5@mail.uc.edu>
2015-09-22 18:47:43 -0500

    passed lesson one, two and zero

--- 01LessonOne/LessonOne.js
+++ 01LessonOne/LessonOne.js
@@ -1,24 +1,28 @@
 module.exports = function() {
 
   this.returnTrue = function () {
-    // should return true
+    return true;
 
   }
 
   this.returnFalse = function() {
-    // should return false
+    return false;
 
   }
 
   this.greaterThan = function (big, small) {
     // should return true if the first argument is greater than the second argument
+    if(big>small) {return true;}
     // and return false if the first argument is less than the second argument
+    else {return false;}
 
   }
 
   this.lessThan = function (small, big) {
     // should return true if the first argument is less than the second argument
+    if (small<big) {return true;}
     // and return false if the first argument is greater than the second argument
+    else {return false;}
 
   }
 
@@ -32,6 +36,6 @@ module.exports = function() {
   this.notEqualTo = function (a, b) {
     // should return true if the first argument is not equal to the second argument
     // and should return false if the first argument is equal to the second argument
-
+    return a!==b;
   }
 }
--- 02LessonTwo/LessonTwo.js
+++ 02LessonTwo/LessonTwo.js
@@ -5,10 +5,24 @@ module.exports = function() {
     // should return "cat" when character is 'garfield'
     // should return "fish" when character is 'nemo'
     // should return false if character is anything else
+    if(character==='scooby') {
+      return 'dog';
+    }
+    else if(character==='garfield') {
+      return 'cat';
+    }
+    else if(character==='nemo') {
+      return 'fish';
+    }
+    else {
+      return false;
+    }
   }
 
   this.isEven = function (number) {
     // should return true is number is even (divisible by 2)
+    if(number%2===0) {
+      return true;
+    }
   }
-
 }
