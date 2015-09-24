d05eb19fbe88b9fb9b769974fa51746256a2458a
Adora Gillespie <adora.mae.jeanne@gmail.com>
2015-09-23 21:30:08 -0500

    Test 3 done

--- 03LessonThree/LessonThree.js
+++ 03LessonThree/LessonThree.js
@@ -3,32 +3,41 @@ module.exports = function() {
   this.buildArray = function (first, second, third) {
     // return array with the items first, second, and third in an array.
 
+    return [first, second, third];
+
   }
 
   this.returnThirdItem = function (arr) {
     // should return the third item in the array
 
+    return arr[2];
   }
 
   this.setFirstItem = function (arr, newFirstItem) {
     // should set the first item in the array with newFirstItem
-
+    arr[0] = newFirstItem;
+    return arr;
   }
 
   this.returnCenterItem = function(fiveByFiveArray) {
     // returns the "center" item in a 5 x 5 array
     // ex. 3 x 3 array [[1, 2, 3], [4, 5, 6], [7, 8, 9]] the center item is 5
 
+    return fiveByFiveArray[2][2];
+
   }
 
   this.arrayJoin = function(arr) {
     // should return a string of the joined array items, separated by a space
 
+    return arr.join(' ');
+
   }
 
   this.stringSplit = function(str) {
     // should return an array of the words in a string, delimited by a space
-
+  
+    return str.split(' ');
   }
 
 }
