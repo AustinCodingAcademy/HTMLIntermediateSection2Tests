d92e251d66d371e7e624cfefb1f98193db5374fa
Chen Yang <yangc5@mail.uc.edu>
2015-10-06 18:35:57 -0500

    lesson5 passed

--- 05LessonFive/LessonFive.js
+++ 05LessonFive/LessonFive.js
@@ -2,12 +2,22 @@ module.exports = function() {
 
   this.loopIt = function () {
     // should return an array with numbers 0 - 99
-
+    var arr=[];
+    for(var i=0; i<100; i++) {
+      arr.push(i);
+    }
+    return arr;
   }
 
   this.onlyEvens = function () {
     // should return an array with all even numbers between 1 - 99
-
+    var arr=[];
+    for(var i=1; i<100; i++) {
+      if(i%2===0){
+        arr.push(i);
+      }
+    }
+    return arr;
   }
 
   this.fizzBuzz = function () {
@@ -15,6 +25,21 @@ module.exports = function() {
     // divisible by 3 are replaced by the string "fizz", numbers divisible by 5
     // are replaced by the string "buzz", and the numbers divisble by both 3 and
     // 5 are replaced by fizzbuzz
-
+    var arr=[];
+    for(var i=0; i<100; i++) {
+      if(i%15===0) {
+        arr.push('fizzbuzz');
+      }
+      else if(i%3===0) {
+        arr.push('fizz');
+      }
+      else if(i%5===0) {
+        arr.push('buzz');
+      }
+      else {
+        arr.push(i);
+      }
+    }
+    return arr;
   }
 }
