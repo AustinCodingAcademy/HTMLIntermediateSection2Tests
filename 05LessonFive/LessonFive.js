0e7d8cdb9843e9acb147bb1da6b1dffcce6f6de6
Jace Deloney <jacedeloney@gmail.com>
2015-10-08 17:47:01 -0500

    Updating Tests

--- 05LessonFive/LessonFive.js
+++ 05LessonFive/LessonFive.js
@@ -1,20 +1,43 @@
-module.exports = function() {
+this.loopIt = function () {
+     // should return an array with numbers 0 - 99
+    var arr = [];
 
-  this.loopIt = function () {
-    // should return an array with numbers 0 - 99
+    for(var i = 0; i < 100; i++){
+      arr.push(i);
+    }
+    return arr;
+   }
 
+   this.onlyEvens = function () {
+     // should return an array with all even numbers between 1 - 99
+    var arrEven = [];
+ 
+    for( j = 2; j < 100; j++){
+        if (j % 2 === 0) {
+      arrEven.push(i);
+    }
   }
-
-  this.onlyEvens = function () {
-    // should return an array with all even numbers between 1 - 99
-
+    return arrEven;
   }
+ 
+   this.fizzBuzz = function () {
+    var arrFB = [];
 
-  this.fizzBuzz = function () {
-    // should return an array of numbers between 0 - 99 where the numbers
-    // divisible by 3 are replaced by the string "fizz", numbers divisible by 5
-    // are replaced by the string "buzz", and the numbers divisble by both 3 and
-    // 5 are replaced by fizzbuzz
+    for(k = 0; k < 100; k++){
+      if (k % 3 === 0 && k % 5 === 0){
+        arr3.push('fizzbuzz');
+      }
+        else if (k % 3 === 0){
+          arr3.push('fizz');
+      }
+        else if (k % 5 === 0){
+          arr3.push('buzz')
+      }
+        else {
+          arr3.push(k);
+      }
+          }
+    return arr3;    
 
-  }
-}
+   }
+ }
