a3c4aac659857adb1d93b857ce3704880ac67562
Jose Martinez Jr <gambit0326@Joses-MacBook-Air.local>
2015-10-13 18:23:19 -0500

    finished 1-4 test

--- 02LessonTwo/LessonTwo.js
+++ 02LessonTwo/LessonTwo.js
@@ -1,6 +1,16 @@
 module.exports = function() {
 
   this.whichSpecies = function (character) {
+  	if (character === 'scooby') {
+  		return 'dog';
+  	}
+  	if (character === 'garfield') {
+  		return 'cat';
+  	}
+  	if (character === 'nemo') {
+  		return 'fish'
+  	}
+  	return false;
     // should return "dog" when character is 'scooby'
     // should return "cat" when character is 'garfield'
     // should return "fish" when character is 'nemo'
@@ -9,6 +19,10 @@ module.exports = function() {
 
   this.isEven = function (number) {
     // should return true is number is even (divisible by 2)
+    if (number % 2 === 0) {
+    	return true;
+    }
+    	return false;
   }
 
 }
--- 03LessonThree/LessonThree.js
+++ 03LessonThree/LessonThree.js
@@ -2,33 +2,33 @@ module.exports = function() {
 
   this.buildArray = function (first, second, third) {
     // return array with the items first, second, and third in an array.
-
+  return var arr = ['first', 'second', 'third'];
   }
 
   this.returnThirdItem = function (arr) {
     // should return the third item in the array
-
+  console.log(arr[2]);
   }
 
   this.setFirstItem = function (arr, newFirstItem) {
     // should set the first item in the array with newFirstItem
-
+    arr[0] = "newFirstItem";
   }
 
   this.returnCenterItem = function(fiveByFiveArray) {
     // returns the "center" item in a 5 x 5 array
     // ex. 3 x 3 array [[1, 2, 3], [4, 5, 6], [7, 8, 9]] the center item is 5
-
+    var fiveByFiveArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9,], [10, 11, 12], [13, 14, 15]];
+    console.log(fiveByFiveArray[2]);
   }
 
   this.arrayJoin = function(arr) {
     // should return a string of the joined array items, separated by a space
-
+    var myVar1 = arr.join(', ');
   }
 
   this.stringSplit = function(str) {
-    // should return an array of the words in a string, delimited by a space
-
+    var strArray = str.split('');
   }
 
 }
